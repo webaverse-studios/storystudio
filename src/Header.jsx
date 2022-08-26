4fb4c6ec5791e6ff361efaf19a7a7faf86b15ad5
alextitonis <alexandrostitonis@gmail.com>
2022-08-26 09:35:27 +0300

    added button to hide api key

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { getOpenAIKey, setOpenAIKey } from "./utils/openai_utils";
 import { lore_header } from "./lore_header";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 async function getFile() {
   const file = await new Promise((resolve, reject) => {
@@ -22,10 +24,14 @@ export async function download_content(url) {
 }
 
 const Header = ({ data, setData, exportHandler, importHandler }) => {
+  const [apiKeyVisible, setApiKeyVisible] = useState(false);
+
   const handleLoad = async (data, fromUrl = true) => {
     if (fromUrl) {
-      const response = await download_content(data.url)
-      let blob = new Blob([response], { type: "application/x-javascript;base64" });
+      const response = await download_content(data.url);
+      let blob = new Blob([response], {
+        type: "application/x-javascript;base64",
+      });
       // decode blob to a string
       const reader = new FileReader();
       reader.readAsText(blob);
@@ -33,28 +39,42 @@ const Header = ({ data, setData, exportHandler, importHandler }) => {
         let content = reader.result;
         // find the line in content (a long delimited string) that contains import and murmurhash3
         // replace that line with lore_header
-        const contentArray = content.split('\n');
-        const importLineIndex = contentArray.findIndex(line => line.includes('murmurhash3.js'));
-        if(importLineIndex !== -1) {
+        const contentArray = content.split("\n");
+        const importLineIndex = contentArray.findIndex((line) =>
+          line.includes("murmurhash3.js")
+        );
+        if (importLineIndex !== -1) {
           contentArray[importLineIndex] = lore_header;
-          content = contentArray.join('\n');
-          console.log('injected content', content);
+          content = contentArray.join("\n");
+          console.log("injected content", content);
           // convert content back to a blob with the x-javascript base64 type
-          blob = new Blob([content], { type: "application/x-javascript;base64" });
+          blob = new Blob([content], {
+            type: "application/x-javascript;base64",
+          });
         }
 
         const fileUri = await fileToDataUri(blob);
         // fileUri is a base64 javascript document
-        // we want to inject some code into the file before we 
+        // we want to inject some code into the file before we
         const importedFile = await import(fileUri);
-        setData({ base: fileUri, type: "file", funcs: importedFile, url: data.url });
-      }
+        setData({
+          base: fileUri,
+          type: "file",
+          funcs: importedFile,
+          url: data.url,
+        });
+      };
     } else {
       // open a file picker and get the file from disk
       const file = await getFile();
       const fileUri = await fileToDataUri(file);
       const importedFile = await import(fileUri);
-      setData({ base: fileUri, type: "file", funcs: importedFile, url: file.name });
+      setData({
+        base: fileUri,
+        type: "file",
+        funcs: importedFile,
+        url: file.name,
+      });
     }
   };
 
@@ -74,7 +94,9 @@ const Header = ({ data, setData, exportHandler, importHandler }) => {
       reader.readAsDataURL(file);
     });
 
-  useEffect(() => { handleLoad (data) }, [])
+  useEffect(() => {
+    handleLoad(data);
+  }, []);
 
   return (
     <div className="header">
@@ -89,10 +111,13 @@ const Header = ({ data, setData, exportHandler, importHandler }) => {
             className={"baseInput"}
             type="text"
             value={data.url}
-            onChange={(e) => setData({...data, url: e.target.value})}
-            onFocus={(e) => setData({...data, url: e.target.value})}
+            onChange={(e) => setData({ ...data, url: e.target.value })}
+            onFocus={(e) => setData({ ...data, url: e.target.value })}
           />
-          <button className={"baseButton baseButtonUrl"} onClick={() => handleLoad(data, true)}>
+          <button
+            className={"baseButton baseButtonUrl"}
+            onClick={() => handleLoad(data, true)}
+          >
             [From URL]
           </button>
           <button
@@ -106,7 +131,7 @@ const Header = ({ data, setData, exportHandler, importHandler }) => {
           <span className={"baseLabel"}>OpenAI Key: </span>
           <input
             className={"baseInput"}
-            type="input"
+            type={apiKeyVisible ? "input" : "password"}
             defaultValue={getOpenAIKey()}
             onChange={(e) =>
               console.log("change", e.target.value) ||
@@ -114,6 +139,13 @@ const Header = ({ data, setData, exportHandler, importHandler }) => {
             }
             onFocus={(e) => setOpenAIKey(e.target.value)}
           />
+          <button
+            className="entityVisibility"
+            value={apiKeyVisible}
+            onClick={(e) => setApiKeyVisible(!apiKeyVisible)}
+          >
+            {apiKeyVisible ? <VisibilityIcon /> : <VisibilityOffIcon />}
+          </button>
         </div>
         <div className={"importExportButtons"}>
           <button className={"importButton"} onClick={() => importJson()}>
