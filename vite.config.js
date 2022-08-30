06ebc0d4762a45b12dead22c9322fd5a0b1f5b1a
shawticus <shawmakesmagic@gmail.com>
2022-08-30 01:38:41 -0700

    fix vite confgig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+// yarn add --dev @esbuild-plugins/node-globals-polyfill
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+// yarn add --dev @esbuild-plugins/node-modules-polyfill
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+// You don't need to add this to deps, it's included by @esbuild-plugins/node-modules-polyfill
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig(async (command) => {
@@ -11,13 +16,16 @@ export default defineConfig(async (command) => {
 
   const returned = {
     plugins: [react()],
+    resolve: {
+      alias: {
+        buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6', 
+      },
+      },
     optimizeDeps: {
       esbuildOptions: {
         define: {
           global: "globalThis",
-        },
-        plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
-      },
+        }      },
     },
   };
 
