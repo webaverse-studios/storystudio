24a04cb3e1301bb2d9c7a4867079b46dd1e2ad11
Muhammad Abeer <abeersaqib948@gmail.com>
2022-09-02 11:10:29 +0500

    Fixes

--- src/app-context.js
+++ src/app-context.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 export const AppContext = React.createContext({
-    theme: themes.dark,
-    toggleTheme: () => {},
+    defaults: {},
+    toggleContext: () => {},
 });
